5242d6ef2de89e10ff124c66c71856cbcd28fbf0
Craig <craigtipple81@gmail.com>
2023-10-06 15:03:13 +0100

    Completed test to check comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -153,6 +153,15 @@ describe("/api/articles/:article_id", () => {
         .expect(200)
         .then(({ body }) => {
           const article = body.article;
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.body).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("number");
         });
     });
 
